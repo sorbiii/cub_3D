c8690858f95733b6d28e44a8e0fa4c8f64d85399
mzapora <zapi.pl@wp.pl>
2026-01-11 15:17:08 +0100

    test in main deleted

--- main.c
+++ main.c
@@ -3,19 +3,5 @@
 
 int main(void)
 {
-	char *map[] = {
-        " 1111111",
-        " 1010001",
-        " 10S0001",
-        " 1111111",
-        NULL
-    };
-    t_data *test;
-    test = malloc(sizeof(t_data));
-    test->map = map;
-    test->m_heigh = 4;
-    test->m_witdh = 7;
-    test->map = copy_map(test);
-    map_checker(test);
 	return (0);
 }
